431ab80b4da99b5427a39e9ea6d8a19e98cf9e38
Surender Kumar <surenderkumar852456@gmail.com>
2026-06-04 11:28:08 +0530

    Delete Image Compressor directory

--- Image Compressor/C/Image Compressor/Fix-It.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-copy "%~dp1%~n1%~x1" "C:\Image Compressor\Backup-Copies\%~n1-Fix-It-%random%-%date%%~x1"
-
-"C:\Image Compressor\ffmpeg\ffmpeg.exe" -i "%~dp1%~n1%~x1"  -loglevel quiet -y "%~dp1%~n1%~x1"
-
-exit
